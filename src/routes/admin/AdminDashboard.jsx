0434b475c0c076f5cf8945b9256b0df17546b11e
Berry1196 <Sebastianberry11@gmail.com>
2023-06-14 03:07:00 +0200

    fixed typo

--- src/routes/admin/AdminDashboard.jsx
+++ src/routes/admin/AdminDashboard.jsx
@@ -108,7 +108,7 @@ export default function AdminHome() {
       <div className="px-4 sm:px-6 lg:px-8">
         <div className="sm:flex sm:items-center">
           <div className="sm:flex-auto">
-            <h1 className="text-base font-semibold leading-6 text-gray-900">Users</h1>
+            <h1 className="text-base font-semibold leading-6 text-gray-900">Events</h1>
             <p className="mt-2 text-sm text-gray-700">A list of all events.</p>
           </div>
           <div className="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
